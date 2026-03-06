48c30de0a6a89ad4dfd3ca1a1441690fc005b5a2
LTost <tollud@nrg.no>
2026-03-06 22:19:22 +0100

    boma

--- server.py
+++ server.py
@@ -1,40 +1,39 @@
 from flask import Flask, render_template, redirect, request
- 
+
 app = Flask(__name__)
- 
+
 @app.route("/Main")
 def home():
     return render_template("Main.html")
- 
+
 @app.route("/Username-input")
 def login():
     return render_template("Username input.html")
- 
+
 @app.route("/General-Recommendations")
 def general():
-    from get_recommendations import get_games, ask_chat
-    username = request.cookies.get("steamUsername")
- 
-    games = get_games(username)
-    recommendations = ask_chat(games)
- 
-    return render_template("General.html", recommendations)
- 
+     return render_template("General.html")
+
 @app.route("/Specific-Recommendations")
 def specific():
-    return render_template("Specific.html")
- 
-@app.route("/How-to")
-def howto():
-    return render_template("Howto.html")
- 
-@app.route("/")
+
+@@ -32,9 +26,19 @@
 def root():
     return redirect("/Main")
- 
+
+@app.route("/Recommended-Games")
+def recommend_games():
+    from get_recommendations import get_games, ask_chat
+
+    username = request.cookies.get("steamUsername")
+    games = get_games(username)
+    recommendations = ask_chat(games)
+
+    return render_template("Recommended games.html", games=recommendations)
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
- 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
