84453a8719cd4603343ec86710b27ddc6f72a542
LTost <tollud@nrg.no>
2026-03-06 23:33:20 +0100

    Update server.py

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ def recommend_games():
     username = request.cookies.get("steamUsername")
     games = get_games(username)
     recommendations = ask_chat(games)
-    recommendations = html.unescape(recommendations)
+    #recommendations = html.unescape(recommendations)
 
     return render_template("Recommended games.html", games=recommendations)
 
